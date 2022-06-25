b1c744f7b78350c0864dff1c18fde939516fcb47
Miguel <chanampamiguel@gmail.com>
2022-06-25 13:39:52 -0300

    Prueba listar personajes

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+List<Personaje> listadoPersonajes = new List<Personaje>();
+
+var random = new Random();
+
+Console.Write("Ingresa la cantidad de personajes a pelear (Entre 1 y 13): ");
+int cantidadPersonajes = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < cantidadPersonajes; i++) {
+  listadoPersonajes.Insert(i, new Personaje());
+};
+
+foreach (Personaje personaje in listadoPersonajes) {
+  Console.WriteLine();
+  personaje.mostrarPersonaje();
+  Console.WriteLine();
+}
