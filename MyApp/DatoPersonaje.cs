7a4703cf2c1c10efc7af7f219f5285fbd5384631
Miguel <chanampamiguel@gmail.com>
2022-06-25 13:38:39 -0300

    Datos Personaje

---
+++ MyApp/DatoPersonaje.cs
@@ -0,0 +1,24 @@
+public class Dato {
+  private int velocidad;// 1 a 10
+  private int destreza; // 1 a 5
+  private int fuerza;// 1 a 10
+  private int nivel; // 1 a 10
+  private int armadura; // 1 a 10
+
+  public Dato() {
+
+    var random = new Random();
+
+    this.velocidad = random.Next(1, 11);
+    this.destreza = random.Next(1, 6);
+    this.fuerza = random.Next(1, 11);
+    this.nivel = random.Next(1, 11);
+    this.armadura = random.Next(1, 11);
+  }
+
+  public int Velocidad { get => velocidad; set => velocidad = value; }
+  public int Destreza { get => destreza; set => destreza = value; }
+  public int Fuerza { get => fuerza; set => fuerza = value; }
+  public int Nivel { get => nivel; set => nivel = value; }
+  public int Armadura { get => armadura; set => armadura = value; }
+}
