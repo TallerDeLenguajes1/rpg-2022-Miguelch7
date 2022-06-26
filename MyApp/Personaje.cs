74a0c74cd3491b3222e9ffd977bd35fc670ac190
Miguel <chanampamiguel@gmail.com>
2022-06-26 20:08:49 -0300

    Agregar metodo atacar

--- MyApp/Personaje.cs
+++ MyApp/Personaje.cs
@@ -30,4 +30,21 @@ public class Personaje {
     this.mostrarCaracteristicas();
   }
 
+  public void atacar(Personaje personaje) {
+
+    var random = new Random();
+
+    int PoderDeDisparo = this.datos.Destreza * this.datos.Fuerza * this.datos.Nivel;
+    double EfectividadDeDisparo = random.Next(1, 101);
+    double ValorDeAtaque = (PoderDeDisparo * EfectividadDeDisparo)/100;
+
+    int PoderDeDefensa = personaje.datos.Armadura * personaje.datos.Velocidad;
+
+    int MaximoDanioProvocable = 5000;
+
+    double DanioProvocado = ( Math.Abs( (((ValorDeAtaque * EfectividadDeDisparo) - PoderDeDefensa) / MaximoDanioProvocable) * 100 ) );
+
+    personaje.caracteristicas.Salud = Convert.ToInt32(personaje.caracteristicas.Salud - DanioProvocado);
+  }
+
 }
