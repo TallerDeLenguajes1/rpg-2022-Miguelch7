8dc2c3ab168e6fced0340f515f4c3a8f489e6f8e
Miguel <chanampamiguel@gmail.com>
2022-06-26 21:11:04 -0300

    Simular torneo tipo battle royale

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -10,8 +10,46 @@ for (int i = 0; i < cantidadPersonajes; i++) {
   listadoPersonajes.Insert(i, new Personaje());
 };
 
-foreach (Personaje personaje in listadoPersonajes) {
-  Console.WriteLine();
-  personaje.mostrarPersonaje();
-  Console.WriteLine();
+Console.WriteLine("Listado de personajes");
+listarPersonajes(listadoPersonajes);
+
+simularTorneo(listadoPersonajes);
+
+
+// ========== FUNCIONES ==========
+void listarPersonajes(List<Personaje> listadoPersonajes) {
+  
+  int indice = 0;
+  
+  foreach (Personaje personaje in listadoPersonajes) {
+    Console.WriteLine($"{ indice }) \"{ personaje.caracteristicas.Apodo }\" { personaje.caracteristicas.Nombre }");
+    indice++;
+  };
+}
+
+void simularTorneo(List<Personaje> listadoPersonajes) {
+
+  Personaje luchador;
+  Personaje retador;
+  Personaje campeon = listadoPersonajes.First();
+  Combate combate;
+  
+  while (listadoPersonajes.Count() > 1) {
+    luchador = listadoPersonajes.ElementAt(0);
+    retador = listadoPersonajes.ElementAt(1);
+
+    combate = new Combate(luchador, retador);
+
+    Console.WriteLine($"\nLa siguiente pelea es entre { luchador.caracteristicas.Nombre } y { retador.caracteristicas.Nombre }");
+
+    Console.WriteLine("FIGHT!");
+    combate.simularPelea();
+
+    Console.WriteLine($"El GANADOR del combate es: { combate.Ganador?.caracteristicas.Nombre }");
+    campeon = combate.Ganador;
+    listadoPersonajes.Remove(combate.Perdedor);
+    combate.mejorarAtributos();
+  };
+
+  Console.WriteLine($"\nEl CAMPEÓN del torneo es: { campeon.caracteristicas.Nombre }");
 }
