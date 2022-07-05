df91596bff6e9844b56dfdfeae6b886752fefd48
Miguel <chanampamiguel@gmail.com>
2022-07-04 21:07:37 -0300

    Guardar ganadores en csv

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,6 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 List<Personaje> listadoPersonajes = new List<Personaje>();
 
+string ganadoresCSV = Directory.GetCurrentDirectory() + "\\ganadores.csv";
+
+if ( !File.Exists(ganadoresCSV) ) {
+  File.Create(ganadoresCSV);
+};
+
+if (File.Exists(ganadoresCSV)) {
+  listarGanadores(ganadoresCSV);
+}
+
 var random = new Random();
 
 Console.Write("Ingresa la cantidad de personajes a pelear (Entre 1 y 13): ");
@@ -13,8 +23,7 @@ for (int i = 0; i < cantidadPersonajes; i++) {
 Console.WriteLine("Listado de personajes");
 listarPersonajes(listadoPersonajes);
 
-simularTorneo(listadoPersonajes);
-
+simularTorneo(listadoPersonajes, ganadoresCSV);
 
 // ========== FUNCIONES ==========
 void listarPersonajes(List<Personaje> listadoPersonajes) {
@@ -27,11 +36,12 @@ void listarPersonajes(List<Personaje> listadoPersonajes) {
   };
 }
 
-void simularTorneo(List<Personaje> listadoPersonajes) {
+void simularTorneo(List<Personaje> listadoPersonajes, string ganadoresCSV) {
 
   Personaje luchador;
   Personaje retador;
   Personaje campeon = listadoPersonajes.First();
+  int cantidadParticipantes = listadoPersonajes.Count();
   Combate combate;
   
   while (listadoPersonajes.Count() > 1) {
@@ -46,10 +56,39 @@ void simularTorneo(List<Personaje> listadoPersonajes) {
     combate.simularPelea();
 
     Console.WriteLine($"El GANADOR del combate es: { combate.Ganador?.caracteristicas.Nombre }");
-    campeon = combate.Ganador;
-    listadoPersonajes.Remove(combate.Perdedor);
-    combate.mejorarAtributos();
+    campeon = combate?.Ganador!;
+    listadoPersonajes.Remove(combate?.Perdedor!);
+    combate?.mejorarAtributos();
   };
 
-  Console.WriteLine($"\nEl CAMPEÓN del torneo es: { campeon.caracteristicas.Nombre }");
+  Console.WriteLine($"\nEl CAMPEÓN del torneo es: { campeon.caracteristicas.Nombre! }");
+
+  guardarCampeon(campeon, cantidadParticipantes, ganadoresCSV);
+}
+
+void guardarCampeon(Personaje ganador, int cantidadParticipantes, string archivoCSV) {
+
+  StreamWriter sw = new StreamWriter(archivoCSV, append: true);
+
+  sw.WriteLine($"{ ganador.caracteristicas.Nombre } { ganador.caracteristicas.Apodo },{ cantidadParticipantes },{ DateTime.Now.ToShortDateString() }");
+
+  sw.Close();
+}
+
+void listarGanadores(string archivoCSV) {
+  StreamReader sr = new StreamReader(archivoCSV);
+
+  string linea = sr.ReadLine()!;
+
+  Console.WriteLine("---------- Listado de ganadores ----------");
+
+  while (linea != null) {
+    Console.WriteLine(linea);
+
+    linea = sr.ReadLine()!;
+  };
+
+  Console.WriteLine();
+
+  sr.Close();
 }
